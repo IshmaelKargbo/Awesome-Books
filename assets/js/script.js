be7db499b28ffdd77411dc92b98d8b1981b77f79
Ishmael Kargbo <ishmaelkargbo5@outlook.com>
2022-04-18 10:19:42 +0000

    Implement storage feature
    1. Read all existing data from the localstorage
    2. Save new books to the storage
    3. Delete a book from the storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,62 @@
 const form = document.getElementById('form');
 const books = document.getElementById('books');
 
-let bookStore = [];
-
-const refresh = () => {
-    books.innerHTML = '';
-
-    bookStore.forEach((e, i) => {
-        const bookCard = document.createElement('div');
-
-        const title = document.createElement('p');
-        title.innerText = e.title;
-        bookCard.appendChild(title);
-
-        const author = document.createElement('p');
-        author.innerText = e.author;
-        bookCard.appendChild(author);
-
-        const remove = document.createElement('button');
-        remove.type = 'button';
-        remove.innerText = 'Remove';
-        remove.onclick = () => {
-            removeBook(i);
-        }
-        bookCard.appendChild(remove);
-
-        const borderB = document.createElement('hr');
-        bookCard.appendChild(borderB);
-
-        books.appendChild(bookCard);
-
-        form.reset()
-    })
+function fetchBooks() {
+  const data = localStorage.getItem('awssome_books');
+  return JSON.parse(data) || [];
 }
 
-const removeBook = (index) => {
-    const temp = bookStore.filter((_, i) => i !== index);
-    bookStore = temp;
-    refresh()
+let bookStore = fetchBooks();
+
+function saveBook(book) {
+  bookStore.push(book);
+  localStorage.setItem('awssome_books', JSON.stringify(bookStore));
 }
 
-form.onsubmit = (e) => {
-    e.preventDefault();
-
-    const data = Object.fromEntries(new FormData(e.target).entries());
-
-    bookStore.push(data);
-
-    refresh();
+function removeBook(index) {
+  const temp = bookStore.filter((_, i) => i !== index);
+  bookStore = temp;
 }
 
+function renderBooks() {
+  books.innerHTML = '';
+
+  bookStore.forEach((e, i) => {
+    const bookCard = document.createElement('div');
+
+    const title = document.createElement('p');
+    title.innerText = e.title;
+    bookCard.appendChild(title);
+
+    const author = document.createElement('p');
+    author.innerText = e.author;
+    bookCard.appendChild(author);
+
+    const remove = document.createElement('button');
+    remove.type = 'button';
+    remove.innerText = 'Remove';
+    remove.onclick = () => {
+      removeBook(i);
+      renderBooks();
+    };
+    bookCard.appendChild(remove);
+
+    const borderB = document.createElement('hr');
+    bookCard.appendChild(borderB);
+
+    books.appendChild(bookCard);
+
+    form.reset();
+  });
+}
+
+renderBooks();
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const book = Object.fromEntries(new FormData(e.target).entries());
+
+  saveBook(book);
+  renderBooks();
+});
