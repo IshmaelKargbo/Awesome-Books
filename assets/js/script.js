4a7c75770cede937cd28c4e95c627402aba2d972
Ishmael Kargbo <ishmaelkargbo5@outlook.com>
2022-04-20 09:02:12 +0000

    Added css style

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,20 +4,28 @@ const awesome = new AwesomeBooks();
 
 const form = document.getElementById('form');
 const books = document.getElementById('books');
+const shelf = document.getElementById('shelf');
 
 function renderBooks() {
+  shelf.innerHTML = '';
   books.innerHTML = '';
 
-  awesome.shelf().forEach((e) => {
-    const bookCard = document.createElement('div');
+  if (awesome.shelf().length === 0) {
+    const no = document.createElement('p');
+    no.classList.add('empty');
+    no.innerHTML = 'Empty Shelf<br >Please add you books';
+    shelf.appendChild(no);
+  }
 
-    const title = document.createElement('p');
-    title.innerText = e.title;
-    bookCard.appendChild(title);
+  awesome.shelf().forEach((e, i) => {
+    const bookCard = document.createElement('li');
 
-    const author = document.createElement('p');
-    author.innerText = e.author;
-    bookCard.appendChild(author);
+    if (i % 2 === 1) bookCard.classList.add('draf');
+
+    const details = document.createElement('p');
+    details.innerText = `"${e.title}" by ${e.author}`;
+
+    bookCard.appendChild(details);
 
     const remove = document.createElement('button');
     remove.type = 'button';
@@ -28,13 +36,12 @@ function renderBooks() {
     };
     bookCard.appendChild(remove);
 
-    const borderB = document.createElement('hr');
-    bookCard.appendChild(borderB);
-
     books.appendChild(bookCard);
 
     form.reset();
   });
+
+  shelf.appendChild(books);
 }
 
 renderBooks();
